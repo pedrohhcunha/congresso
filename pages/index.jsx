6a378cbe722aafebc6df0a10fce8dc4e27bc954a
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-23 10:04:52 -0300

    Criando componentes a serem usados na página inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,16 @@
+import Header from '../components/Header.jsx'
+import Inicio from '../components/Inicio.jsx'
+import Sobre from '../components/Sobre.jsx'
+import Diferenciais from '../components/Diferenciais.jsx'
+import Cronograma from '../components/Cronograma.jsx'
+import Participe from '../components/Participe.jsx'
+import Palestras from '../components/Palestras.jsx'
+import ParticipeB from '../components/ParticipeB.jsx'
+import Parceiros from '../components/Parceiros.jsx'
+import Cadastro from '../components/Cadastro.jsx'
+import Mapa from '../components/Mapa.jsx'
+import Footer from '../components/Footer.jsx'
+
 export default function Home() {
   return (
     <main>
