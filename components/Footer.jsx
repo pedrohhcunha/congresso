d02a9c77674832892aa2ba1c0fd9450577958544
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-24 15:09:20 -0300

    Mudando a tipologia do link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -27,7 +27,17 @@ export default function Footer(props) {
             <div className={styles.realFooter}>
                 <h5 className={styles.span}>© 2021 Azeplast. Todos os direitos reservados.</h5>
                 <h5 className={styles.span}>
-                    <a href="/">Termos e condições</a> | <a href="/">Politica de privacidade</a> | <a href="/">Sitemap</a>
+                    <Link href="/">
+                        <a>Termos e condições</a>
+                    </Link>
+                    |
+                    <Link href="/">
+                        <a>Politica de privacidade</a>
+                    </Link>
+                    |
+                    <Link href="/">
+                        <a>Sitemap</a>
+                    </Link>
                 </h5>
             </div>
         </footer>
