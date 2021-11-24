ac130d4b410ec62624cecee37e7a13f849e5b374
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-24 14:48:48 -0300

    Corrigindo erro de build

--- components/Inicio.jsx
+++ components/Inicio.jsx
@@ -4,7 +4,7 @@ export default function Inicio(props) {
         <section id="Inicio" className={styles.section}>
             <div className={styles.areaContent}>
                 <div className={styles.areaNome}>
-                    <h1 className={styles.numero}>1°</h1>
+                    <h1 className={styles.numero}>{"1°"}</h1>
                     <h1 className={styles.titulo}>Congresso <br /> Sul brasileiro <br /> de Inovação em Saúde</h1>
                 </div>
                 <h5 className={styles.span}>"Security, technology and sustainability in infection prevention and control."</h5>
