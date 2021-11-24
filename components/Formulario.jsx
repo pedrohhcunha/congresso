9997121e0d09eeb0eeac5662c82d79e150f952c8
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-24 09:28:42 -0300

    Coletando informações do formulário de contato

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -1,31 +1,48 @@
+import { useState } from 'react'
 import styles from '../styles/Formulario.module.scss'
 
 export default function Formulario(props) {
+
+    const [dataForm, setDataForm] = useState({
+        nome: "",
+        email: "",
+        telefone: "",
+        profissao: ""
+    })
+
+    const handleInput = event => {
+        setDataForm({...dataForm, [event.target.name]: event.target[event.target.selectedIndex] ? event.target[event.target.selectedIndex].value : event.target.value})
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        console.log(dataForm)
+    }
+
     return(
-        <form className={styles.form} onSubmit={event => event.preventDefault()}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 1</label>
-                <input type="text" name="campo1" className={styles.input} />
+                <label className={styles.label}>Nome</label>
+                <input onChange={event => handleInput(event)} type="text" name="nome" className={styles.input} />
             </div>
             <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 2</label>
-                <input type="text" name="campo2" className={styles.input} />
+                <label className={styles.label}>Email</label>
+                <input onChange={event => handleInput(event)} type="email" name="email" className={styles.input} />
             </div>
             <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 1</label>
-                <input type="text" name="campo1" className={styles.input} />
+                <label className={styles.label}>Telefone</label>
+                <input onChange={event => handleInput(event)} type="text" name="telefone" className={styles.input} />
             </div>
             <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 2</label>
-                <input type="text" name="campo2" className={styles.input} />
-            </div>
-            <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 1</label>
-                <input type="text" name="campo1" className={styles.input} />
-            </div>
-            <div className={styles.areaInput}>
-                <label className={styles.label}>Campo 2</label>
-                <input type="text" name="campo2" className={styles.input} />
+                <label className={styles.label}>Profissão</label>
+                <select onChange={event => handleInput(event)} className={styles.input} name="profissao" id="">
+                    <option value="Selecione">Selecione:</option>
+                    <option value="Estudante1">Estudante de curso Técnico, Tecnólogo ou Graduação</option>
+                    <option value="Estudante2">Estudante de curso de Pós-Graduação</option>
+                    <option value="Médico">Médico(a)</option>
+                    <option value="Enfermeiro">Enfermeiro(a)</option>
+                    <option value="Outro">Outro(a)</option>
+                </select>
             </div>
             <button className={styles.button}>Participar</button>
         </form>
