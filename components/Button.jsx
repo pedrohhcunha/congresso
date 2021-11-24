0cf7797a25be7f300f40e07ae241b8a59c66ab97
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-24 09:40:22 -0300

    Fazendo CTA redirecionarem para a section de cadastro

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,10 @@
 import styles from '../styles/Button.module.scss'
+import Link from 'next/link'
 
 export default function Button(props) {
     return(
-        <button className={styles.button}>{props.children}</button>
+        <Link href="/#Cadastro">
+            <a className={styles.button}>{props.children}</a>
+        </Link>
     )
 }
--- components/Cadastro.jsx
+++ components/Cadastro.jsx
@@ -3,7 +3,7 @@ import Formulario from './Formulario.jsx'
 
 export default function Cadastro(props) {
     return(
-        <section className={styles.section}>
+        <section id="Cadastro" className={styles.section}>
             <h2 className={styles.titulo}>Cadastre-se agora</h2>
             <div className={styles.areaForm}>
                 <Formulario />
