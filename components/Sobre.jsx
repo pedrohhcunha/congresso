02d3031d8654831e1d4724144045a2207f09d285
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-24 15:06:54 -0300

    Adiconando atributos alt nas imagens

--- components/Sobre.jsx
+++ components/Sobre.jsx
@@ -7,7 +7,7 @@ export default function Sobre(props) {
     return(
         <section id="Sobre" className={styles.section}>
             <div className={styles.areaImagem}>
-                <Image layout="fill" src={placeholder} objectFit="cover"/>
+                <Image alt="Imagem sobre a infecção hospitalar" layout="fill" src={placeholder} objectFit="cover"/>
             </div>
             <div className={styles.areaInfo}>
                 <p className={styles.p}>Quanto maior o tempo de permanência nas unidades de saúde, maiores serão as chances de riscos de contaminação, principalmente em hospitais que tratam de doenças crônicas. As infecções mais comuns, geralmente, são a urinária e a do trato respiratório, ocorrendo, geralmente, após as cirurgias. Para combater estes problemas, a Agência Nacional de Vigilância Sanitária (Anvisa) criou protocolos de prevenção e controle para os hospitais brasileiros. Além de segui-los com bastante rigor, o Hospital Santa Lúcia também cria, constantemente, campanhas de conscientização para seu corpo médico e visitantes, com o objetivo de alertá-los sobre cuidados com a infecção hospitalar.</p>
