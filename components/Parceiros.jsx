4c1151b878f929e4b2fbf293cd91c300b8bf0195
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-25 09:18:40 -0300

    Nossos parceiros

--- components/Parceiros.jsx
+++ components/Parceiros.jsx
@@ -1,45 +1,57 @@
 import styles from '../styles/Parceiros.module.scss'
-import ufg from '../public/images/parceiros/ufg.png'
+import qualis from '../public/images/parceiros/qualis.png'
+import unimed from '../public/images/parceiros/unimed.png'
+import hsvp from '../public/images/parceiros/hsvp.png'
+import hrsp from '../public/images/parceiros/hrsp.png'
+import uffs from '../public/images/parceiros/uffs.png'
+import unochapeco from '../public/images/parceiros/unochapeco.png'
+import udesc from '../public/images/parceiros/udesc.png'
+import fapesc from '../public/images/parceiros/fapesc.png'
 import Image from 'next/image'
 
 export default function Parceiros(props) {
     return(
         <section id="Parceiros" className={styles.section}>
-            <h2 className={styles.titulo}>Texto da seção</h2>
+            <h2 className={styles.titulo}>Conheça os apoiadores do evento</h2>
             <div className={styles.parceiros}>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da QUALIS" src={qualis} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://www.unimed.coop.br/site/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da UNIMED" src={unimed} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://hsvp.org.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial do HSVP" src={hsvp} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://www.hrsp.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial do HRSP" src={hrsp} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://www.uffs.edu.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da UFFS" src={uffs} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://www.unochapeco.edu.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da UNOCHAPECO" src={unochapeco} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.ufg.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo da empresa parceira do evento" src={ufg} className={styles.image} />
+                    <a href="https://www.udesc.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da UDESC" src={udesc} className={styles.image} />
+                    </a>
+                </div>
+                <div className={styles.parceiro}>
+                    <a href="https://www.fapesc.sc.gov.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da FAPESC" src={fapesc} className={styles.image} />
                     </a>
                 </div>
             </div>
