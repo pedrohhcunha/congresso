a10ae8dd2d5d48edc877ed4cd6e4869fb0e0bc48
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-26 09:58:48 -0300

    Alterando o texto do botao de cta

--- components/ParticipeB.jsx
+++ components/ParticipeB.jsx
@@ -5,7 +5,7 @@ export default function ParticipeB(props) {
     return(
         <section className={styles.section}>
             <h2 className={styles.titulo}> Garanta a sua vaga no congresso! </h2>
-            <Button>CTA BUTTON</Button>
+            <Button>GARANTIR MINHA VAGA</Button>
         </section>
     )
 }
