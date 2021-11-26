0b034bc3591856b880cc8832a365b0278f5eeb53
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-26 09:37:46 -0300

    Adicionando o conteúdo para a seção de submissão de trabalhos

--- components/Trabalhos.jsx
+++ components/Trabalhos.jsx
@@ -8,15 +8,16 @@ export default function Trabalhos(props) {
             <div className={styles.content}>
                 <div className={styles.leftArea}>
                     <p className={styles.p}>
-                    -  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam reprehenderit sequi recusandae rem alias, sit accusantium rerum id voluptatibus atque esse nam cumque. <br />
-                    -  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam reprehenderit sequi recusandae rem alias, sit accusantium rerum id voluptatibus atque esse nam cumque. <br />
-                    -  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam reprehenderit sequi recusandae rem alias, sit accusantium rerum id voluptatibus atque esse nam cumque. <br />
-                    -  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam reprehenderit sequi recusandae rem alias, sit accusantium rerum id voluptatibus atque esse nam cumque. <br />
+                    <br /> - A inscrição deverá ser efetuada  obrigatoriamente por um autor já inscrito no evento;
+                    <br /> - Deve ser enviado o resumo do trabalho a ser apresentado durante o CONIVAH;
+                    <br /> - Número máximo de autores: cinco. Cada autor poderá apresentar até dois trabalhos como relator;
+                    <br /> - Não há limite para submissão de trabalhos como coautor;
+                    <br /> - Não será cobrada taxas ou valores adicionais para a submissão de trabalhos no congresso.
                     </p>
                 </div>
                 <div className={styles.rightArea}>
                     <div className={styles.calendar}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam repellat vitae ullam explicabo accusamus eligendi modi dignissimos ab neque porro iusto totam odio natus, voluptatibus, illum minima, vero aliquam tempore.</div>
-                    <Button>CTA BUTTON</Button>
+                    <Button>Submeter trabalhos</Button>
                 </div>
             </div>
         </section>
