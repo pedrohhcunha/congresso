f36d9cc912afe93649f36707924626d1693a145b
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-11-29 09:15:46 -0300

    Diferenciais

--- components/Diferenciais.jsx
+++ components/Diferenciais.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Diferenciais.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faArrowsAltH, faBong, faBrain } from '@fortawesome/free-solid-svg-icons'
 
 export default function Diferenciais(props) {
     return(
@@ -9,31 +9,24 @@ export default function Diferenciais(props) {
             <div className={styles.diferenciais}>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faBong} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Ciência</h4>
                     <p className={styles.p}>Reunir diferentes atores da prática assistencial à saúde e estimular estudantes e professores da área para ampliar os conhecimentos sobre infecção hospitalar. </p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faArrowsAltH} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Conexão</h4>
                     <p className={styles.p}>Reunir pesquisadores para refletir sobre temas relevantes da área de Infecção Hospitalar, em palestras e apresentações de trabalhos científicos que compõem o evento.</p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faCoffee} />
+                        <FontAwesomeIcon icon={faBrain} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Solução</h4>
                     <p className={styles.p}>Apresentar soluções corporativas para qualificar a prática profissional e melhorar a qualidade da assistência à saúde e a segurança dos profissionais e pacientes.</p>
-                </div>
-                <div className={styles.diferencial}>
-                    <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faCoffee} />
-                    </div>
-                    <h4 className={styles.tituloDiferencial}>Prevenção e controle</h4>
-                    <p className={styles.p}>Com amplo know-how para investir em produtos de alta qualidade, buscamos diariamente entender as necessidades dos nossos clientes e atendê-las na medida certa.</p>
                 </div>
             </div>
         </section>
