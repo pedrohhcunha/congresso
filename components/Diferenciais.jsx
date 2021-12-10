a3abc9b3aacb519b206036ad0f92d64655497a55
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-10 14:22:15 -0300

    Alterando os icones de objetivos

--- components/Diferenciais.jsx
+++ components/Diferenciais.jsx
@@ -1,29 +1,32 @@
 import styles from '../styles/Diferenciais.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faArrowsAltH, faBong, faBrain } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+import ciencia from '../public/icones/ciencia.png'
+import conexao from '../public/icones/conexao.png'
+import solucao from '../public/icones/solucao.png'
+
 
 export default function Diferenciais(props) {
     return(
         <section id="Diferenciais" className={styles.section}>
-            <h2 className={styles.titulo}>Nossos Diferenciais</h2>
+            <h2 className={styles.titulo}>Objetivos</h2>
             <div className={styles.diferenciais}>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faBong} />
+                        <Image src={ciencia} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Ciência</h4>
-                    <p className={styles.p}>Reunir diferentes atores da prática assistencial à saúde e estimular estudantes e professores da área para ampliar os conhecimentos sobre infecção hospitalar. </p>
+                    <p className={styles.p}>Reunir diferentes atores da prática assistencial à saúde e estimular estudantes e professores da área para ampliar os conhecimentos sobre infecção hospitalar.</p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faArrowsAltH} />
+                        <Image src={conexao} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Conexão</h4>
                     <p className={styles.p}>Reunir pesquisadores para refletir sobre temas relevantes da área de Infecção Hospitalar, em palestras e apresentações de trabalhos científicos que compõem o evento.</p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <FontAwesomeIcon icon={faBrain} />
+                        <Image src={solucao} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Solução</h4>
                     <p className={styles.p}>Apresentar soluções corporativas para qualificar a prática profissional e melhorar a qualidade da assistência à saúde e a segurança dos profissionais e pacientes.</p>
