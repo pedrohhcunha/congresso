caf3cbc95ef83059d64c4557df6fe0bff76461c8
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-13 10:59:12 -0300

    Adicionando os apoiadores do evento

--- components/Parceiros.jsx
+++ components/Parceiros.jsx
@@ -1,167 +1,78 @@
 import styles from '../styles/Parceiros.module.scss'
-import qualis from '../public/images/parceiros/qualis.png'
-import unimed from '../public/images/parceiros/unimed.png'
-import hsvp from '../public/images/parceiros/hsvp.png'
-import hrsp from '../public/images/parceiros/hrsp.png'
-import uffs from '../public/images/parceiros/uffs.png'
-import unochapeco from '../public/images/parceiros/unochapeco.png'
-import udesc from '../public/images/parceiros/udesc.png'
-import fapesc from '../public/images/parceiros/fapesc.png'
-import Image from 'next/image'
 
+import septi from '../public/images/parceiros/septi_parceiro.png'
+import azeplast from '../public/images/parceiros/azeplast_parceiro.png'
+import rioquimica from '../public/images/parceiros/rioquimica_parceiro.png'
+import greenbags from '../public/images/parceiros/greenbags_parceiro.png'
+import onfinity from '../public/images/parceiros/onfinity_parceiro.png'
+
+import berry from '../public/images/parceiros/berry_parceiro.png'
+import qualis from '../public/images/parceiros/qualis_parceiro.png'
+
+import Image from 'next/image'
 export default function Parceiros(props) {
     return(
         <section id="Parceiros" className={styles.section}>
             <h2 className={styles.titulo}>Conheça os apoiadores do evento</h2>
+
             <div className={styles.areaSubtitulo}>
                 <h3 className={styles.subtitulo}>Patrocinadores Ouro</h3>
             </div>
-                <div className={styles.parceiros}>
+            <div className={styles.parceiros}>
                 <div className={styles.parceiro}>
-                    <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da QUALIS" src={qualis} className={styles.image} />
+                    <a href="https://septi.life.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da SEPTI HEALTHCARE" src={septi} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.unimed.coop.br/site/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNIMED" src={unimed} className={styles.image} />
+                    <a href="https://www.azeplast.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da AZEPLAT" src={azeplast} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://hsvp.org.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HSVP" src={hsvp} className={styles.image} />
+                    <a href="https://rioquimica.com.br/pt/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da RIOQUIMICA" src={rioquimica} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.hrsp.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HRSP" src={hrsp} className={styles.image} />
+                    <a href="https://onfinity.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da GREEN BAGS" src={greenbags} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.uffs.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UFFS" src={uffs} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.unochapeco.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNOCHAPECO" src={unochapeco} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.udesc.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UDESC" src={udesc} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.fapesc.sc.gov.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da FAPESC" src={fapesc} className={styles.image} />
+                    <a href="https://onfinity.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da ONFINITY" src={onfinity} className={styles.image} />
                     </a>
                 </div>
             </div>
+
             <div className={styles.areaSubtitulo}>
                 <h3 className={styles.subtitulo}>Apoiadores Ouro</h3>
             </div>
             <div className={styles.parceiros}>
                 <div className={styles.parceiro}>
                     <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da QUALIS" src={qualis} className={styles.image} />
+                        <Image alt="Logo oficial da BERRY" src={berry} className={styles.image} />
                     </a>
                 </div>
                 <div className={styles.parceiro}>
-                    <a href="https://www.unimed.coop.br/site/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNIMED" src={unimed} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://hsvp.org.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HSVP" src={hsvp} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.hrsp.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HRSP" src={hrsp} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.uffs.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UFFS" src={uffs} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.unochapeco.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNOCHAPECO" src={unochapeco} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.udesc.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UDESC" src={udesc} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.fapesc.sc.gov.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da FAPESC" src={fapesc} className={styles.image} />
+                    <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
+                        <Image alt="Logo oficial da BERRY" src={qualis} className={styles.image} />
                     </a>
                 </div>
             </div>
+
             <div className={styles.areaSubtitulo}>
                 <h3 className={styles.subtitulo}>Apoiadores Prata</h3>
             </div>
              <div className={styles.parceiros}>
-                <div className={styles.parceiro}>
-                    <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da QUALIS" src={qualis} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.unimed.coop.br/site/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNIMED" src={unimed} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://hsvp.org.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HSVP" src={hsvp} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.hrsp.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial do HRSP" src={hrsp} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.uffs.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UFFS" src={uffs} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.unochapeco.edu.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNOCHAPECO" src={unochapeco} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.udesc.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UDESC" src={udesc} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.fapesc.sc.gov.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da FAPESC" src={fapesc} className={styles.image} />
-                    </a>
-                </div>
+
             </div>
+
             <div className={styles.areaSubtitulo}>
                 <h3 className={styles.subtitulo}>Patrocinadores Bronze</h3>
             </div>
             <div className={styles.parceiros}>
-                <div className={styles.parceiro}>
-                    <a href="https://qualisinfectologia.com.br/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da QUALIS" src={qualis} className={styles.image} />
-                    </a>
-                </div>
-                <div className={styles.parceiro}>
-                    <a href="https://www.unimed.coop.br/site/" rel="noreferrer" target="_blank">
-                        <Image alt="Logo oficial da UNIMED" src={unimed} className={styles.image} />
-                    </a>
-                </div>
             </div>
         </section>
     )
