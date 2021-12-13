493735be1ef654efa9c1020d319542fac2c85dc6
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-13 15:40:56 -0300

    Estruturando o novo footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,26 +4,12 @@ import Link from 'next/link'
 export default function Footer(props) {
     return(
         <footer className={styles.footer}>
-            <ul className={styles.lista}>
-                <li className={styles.itemLista}>
-                    <Link href="/"><a>Inicio</a></Link>
-                </li>
-                <li className={styles.itemLista}>
-                    <Link href="/#Sobre"><a>Sobre</a></Link>
-                </li>
-                <li className={styles.itemLista}>
-                    <Link href="/#Diferenciais"><a>Diferenciais</a></Link>
-                </li>
-                <li className={styles.itemLista}>
-                    <Link href="/#Cronograma"><a>Cronograma</a></Link>
-                </li>
-                <li className={styles.itemLista}>
-                    <Link href="/#Palestrantes"><a>Palestrantes</a></Link>
-                </li>
-                <li className={styles.itemLista}>
-                    <Link href="/#Parceiros"><a>Parceiros</a></Link>
-                </li>
-            </ul>
+            <div className="subFooter">
+                <h4>25, 26 e 27 de Dezembro</h4>
+                <button>
+                    CTA BUTTON
+                </button>
+            </div>
             <div className={styles.realFooter}>
                 <h5 className={styles.span}>© 2021 Azeplast. Todos os direitos reservados.</h5>
                 <h5 className={styles.span}>
