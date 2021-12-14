e1dbb3149c8136684528bf09080ba53dbf89c87d
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-14 09:12:13 -0300

    Melhorando o SEO

--- components/Diferenciais.jsx
+++ components/Diferenciais.jsx
@@ -12,21 +12,21 @@ export default function Diferenciais(props) {
             <div className={styles.diferenciais}>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <Image src={ciencia} />
+                        <Image alt="Icone de diferenciais do congresso" src={ciencia} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Ciência</h4>
                     <p className={styles.p}>Reunir diferentes atores da prática assistencial à saúde e estimular estudantes e professores da área para ampliar os conhecimentos sobre infecção hospitalar.</p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <Image src={conexao} />
+                        <Image alt="Icone de diferenciais do congresso" src={conexao} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Conexão</h4>
                     <p className={styles.p}>Reunir pesquisadores para refletir sobre temas relevantes da área de Infecção Hospitalar, em palestras e apresentações de trabalhos científicos que compõem o evento.</p>
                 </div>
                 <div className={styles.diferencial}>
                     <div className={styles.areaIcon}>
-                        <Image src={solucao} />
+                        <Image alt="Icone de diferenciais do congresso" src={solucao} />
                     </div>
                     <h4 className={styles.tituloDiferencial}>Solução</h4>
                     <p className={styles.p}>Apresentar soluções corporativas para qualificar a prática profissional e melhorar a qualidade da assistência à saúde e a segurança dos profissionais e pacientes.</p>
