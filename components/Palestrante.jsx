262c382a0c6fd795e911a5008c81dddd7ab759f6
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-14 09:10:55 -0300

    Animaçao nos palestrantes

---
+++ components/Palestrante.jsx
@@ -0,0 +1,29 @@
+import styles from '../styles/Palestrante.module.scss'
+import Image from 'next/image'
+import { useState, useEffect } from 'react';
+
+export default function name(props) {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const elementIsVisible = el => {        
+        if(el.getBoundingClientRect().bottom <= window.innerHeight) {
+            setIsVisible(true)
+            console.log("viu")
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => elementIsVisible(document.querySelector(`#palestranteId${props.index}`)));
+    },[]);
+
+    return(
+        <div id={`palestranteId${props.index}`} className={`${styles.palestrante} ${isVisible ? styles.visible : ''}`}>
+            <div className={styles.areaImagem}>
+                <Image className={styles.img} alt="Imagem de perfil do palestrante" src={props.palestrante.foto} layout="fill" />
+            </div>
+            <h4 className={styles.tituloPalestrante}>{props.palestrante.nome}</h4>
+            <h5 className={styles.cargo}>{props.palestrante.cargo}</h5>
+        </div>
+    )
+}
--- components/Palestrantes.jsx
+++ components/Palestrantes.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Palestrantes.module.scss'
 import placeholder from '../public/images/placeholder.png'
-import Image from 'next/image'
+import Palestrante from './Palestrante'
 
 import fabiana from '../public/images/palestrantes/fabiana.jpg'
 import rodrigo from '../public/images/palestrantes/rodrigo.jpeg'
@@ -45,9 +45,21 @@ export default function Palestrantes(prosp) {
                 assunto: 'Gerenciamento de riscos: teoria de sistemas complexos e Safety 1 X Safety 2.',
                 empresa: 'Unochapecó',
                 foto: placeholder,
-            }
-        ],
-        [
+            },
+            {
+                nome: 'Isabela Pisseti Piccinin',
+                cargo: 'Farmacêutica',
+                assunto: 'Estruturação de serviços de gestão de suprimentos em hospitais para garantir a segurança do paciente.',
+                empresa: '',
+                foto: placeholder,
+            },
+            {
+                nome: 'Felipe Dal Pizzol',
+                cargo: 'Professor',
+                assunto: 'Avanços e perspectivas no campo das inovações em saúde: segurança, tecnologia e sustentabilidade em prevenção e controle de infecções hospitalares.',
+                empresa: 'Rioquímica',
+                foto: placeholder,
+            },
             {
                 nome: 'Jorge Sampaio',
                 cargo: 'Professor',
@@ -76,22 +88,7 @@ export default function Palestrantes(prosp) {
                 empresa: 'Rioquimica',
                 foto: placeholder,
             }
-        ],
-        [
-            {
-                nome: 'Isabela Pisseti Piccinin',
-                cargo: 'Farmacêutica',
-                assunto: 'Estruturação de serviços de gestão de suprimentos em hospitais para garantir a segurança do paciente.',
-                empresa: '',
-                foto: placeholder,
-            },
-            {
-                nome: 'Felipe Dal Pizzol',
-                cargo: 'Professor',
-                assunto: 'Avanços e perspectivas no campo das inovações em saúde: segurança, tecnologia e sustentabilidade em prevenção e controle de infecções hospitalares.',
-                empresa: 'Rioquímica',
-                foto: placeholder,
-            }
+
         ]
     ]
 
@@ -100,31 +97,7 @@ export default function Palestrantes(prosp) {
             <h2 className={styles.titulo}>Palestrantes confirmados</h2>
             <div className={styles.palestrantes}>
                 {palestrantes[0].map((palestrante, index) => (
-                    <div key={index} className={styles.palestrante}>
-                        <div className={styles.areaImagem}>
-                            <Image className={styles.img} alt="Imagem de perfil do palestrante" src={palestrante.foto} layout="fill" />
-                        </div>
-                        <h4 className={styles.tituloPalestrante}>{palestrante.nome}</h4>
-                        <h5 className={styles.cargo}>{palestrante.cargo}</h5>
-                    </div>
-                ))}
-                {palestrantes[1].map((palestrante, index) => (
-                    <div key={index} className={styles.palestrante}>
-                        <div className={styles.areaImagem}>
-                            <Image className={styles.img} alt="Imagem de perfil do palestrante" src={palestrante.foto} layout="fill" />
-                        </div>
-                        <h4 className={styles.tituloPalestrante}>{palestrante.nome}</h4>
-                        <h5 className={styles.cargo}>{palestrante.cargo}</h5>
-                    </div>
-                ))}
-                {palestrantes[2].map((palestrante, index) => (
-                    <div key={index} className={styles.palestrante}>
-                        <div className={styles.areaImagem}>
-                            <Image className={styles.img} alt="Imagem de perfil do palestrante" src={palestrante.foto} layout="fill" />
-                        </div>
-                        <h4 className={styles.tituloPalestrante}>{palestrante.nome}</h4>
-                        <h5 className={styles.cargo}>{palestrante.cargo}</h5>
-                    </div>
+                    <Palestrante palestrante={palestrante} index={index} />
                 ))}
             </div>
         </section>
