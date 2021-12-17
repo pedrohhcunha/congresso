5fe6d35b1118efe6a4b1418b0105d2b1989ce963
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-17 16:50:20 -0300

    alterando form

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -7,6 +7,8 @@ export default function Formulario(props) {
     const [dataForm, setDataForm] = useState({
         nome: "",
         email: "",
+        telefone: "",
+        profissao: ""
     })
 
     const handleInput = event => {
@@ -32,6 +34,21 @@ export default function Formulario(props) {
             <div className={styles.areaInput}>
                 <label className={styles.label}>Email</label>
                 <input onChange={event => handleInput(event)} type="email" name="email" className={styles.input} />
+            </div>
+            <div className={styles.areaInput}>
+                <label className={styles.label}>Telefone</label>
+                <input onChange={event => handleInput(event)} type="text" name="telefone" className={styles.input} />
+            </div>
+            <div className={styles.areaInput}>
+                <label className={styles.label}>Profissão</label>
+                <select onChange={event => handleInput(event)} className={styles.input} name="profissao" id="">
+                    <option value="Selecione">Selecione:</option>
+                    <option value="Estudante1">Estudante de curso Técnico, Tecnólogo ou Graduação</option>
+                    <option value="Estudante2">Estudante de curso de Pós-Graduação</option>
+                    <option value="Médico">Médico(a)</option>
+                    <option value="Enfermeiro">Enfermeiro(a)</option>
+                    <option value="Outro">Outro(a)</option>
+                </select>
             </div>
             <button className={styles.button}>Participar</button>
         </form>
