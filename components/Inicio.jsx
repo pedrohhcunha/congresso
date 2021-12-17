b058a8396f20b7df34c4435714db00e50bd355af
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-17 16:22:22 -0300

    Destaque para o CONTIS

--- components/Inicio.jsx
+++ components/Inicio.jsx
@@ -5,7 +5,10 @@ export default function Inicio(props) {
             <div className={styles.areaContent}>
                 <div className={styles.areaNome}>
                     <h1 className={styles.numero}>{"1°"}</h1>
-                    <h1 className={styles.titulo}>CONTIS <br /> Congresso <br /> Sul Brasileiro <br /> de Inovação em Saúde</h1>
+                    <div className={styles.areaRight}>
+                        <h1 className={styles.contis}>CONTIS</h1>
+                        <h1 className={styles.titulo}>Congresso <br /> Sul Brasileiro <br /> de Inovação em Saúde</h1>
+                    </div>
                 </div>
                 <h5 className={styles.span}>{`"Security, Technology and Sustainability in Infection prevention and control"`}</h5>
             </div>
