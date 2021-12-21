ad170b4ff9f996623317ae73c50ef4a592fdd927
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-21 10:17:17 -0300

    Retirando links sem fim

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,24 +1,10 @@
 import styles from '../styles/Footer.module.scss'
-import Link from 'next/link'
 
 export default function Footer(props) {
     return(
         <footer className={styles.footer}>
             <div className={styles.realFooter}>
-                <h5 className={styles.span}>© 2021 Azeplast. Todos os direitos reservados.</h5>
-                <h5 className={styles.span}>
-                    <Link href="/">
-                        <a className={styles.a}>Termos e condições</a>
-                    </Link>
-                    |
-                    <Link href="/">
-                        <a className={styles.a}>Politica de privacidade</a>
-                    </Link>
-                    |   
-                    <Link href="/">
-                        <a className={styles.a}>Sitemap</a>
-                    </Link>
-                </h5>
+                <h5 className={styles.span}>© 2021 CONTIS. Todos os direitos reservados.</h5>
             </div>
         </footer>
     )
