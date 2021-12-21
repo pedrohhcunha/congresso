8cbf061ab1c097a31c9eac0acd61d29407a291d3
pedrohhcunha <pedro.cunha@onfinity.com.br>
2021-12-21 10:35:30 -0300

    Adicinando as redes sociais no footer da aplicação

--- components/SubFooter.jsx
+++ components/SubFooter.jsx
@@ -2,6 +2,10 @@ import styles from '../styles/SubFooter.module.scss'
 import Button from './Button'
 import logo from '../public/images/logos/logoParaFundoBranco.png'
 import Image from  'next/image'
+import Link from 'next/link'
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function SubFooter(props) {
     return(
         <section className={styles.section}>
@@ -9,6 +13,23 @@ export default function SubFooter(props) {
                 <Image src={logo} className={styles.image} />
             </div>
             <h2>25, 26 e 27 de Dezembro</h2>
+            <div className={styles.socialMedia}>
+                <Link href="https://www.instagram.com/contisbrasil/" target="__blank">
+                    <a className={styles.rede}>
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                </Link>
+                <Link href="https://www.facebook.com/contisbrasil" target="__blank">
+                    <a className={styles.rede}>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                </Link>
+                <Link href="https://www.linkedin.com/company/77623447/" target="__blank">
+                    <a className={styles.rede}>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                </Link>
+            </div>
             <Button style={{
                 backgroundColor: 'var(--blue)',
                 color: 'var(--white)',
