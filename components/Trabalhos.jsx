53f9563e74b36ad57d8b264aee8e1dc758190f61
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-17 08:21:10 -0300

    Adding new button to fowload a file

--- components/Trabalhos.jsx
+++ components/Trabalhos.jsx
@@ -40,6 +40,9 @@ export default function Trabalhos(props) {
             <div className={styles.link}>
                 <a target="__blank" className={styles.a} href="template.pptx">Acesse aqui o template do pôster para apresentação.</a>
             </div>
+            <div className={styles.link}>
+                <a target="__blank" className={styles.a} href="regras.pdf">Acesse aqui as regras para apresentação e avaliação do vídeo.</a>
+            </div>
         </section>
     )
 }
