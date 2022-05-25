9bd881456207954be8c51c5a5181e76f82c8634a
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-25 14:40:51 -0300

    Adicionandno video no site do contis"

--- components/Sobre.jsx
+++ components/Sobre.jsx
@@ -2,13 +2,12 @@ import styles from '../styles/Sobre.module.scss'
 import Button from './Button'
 import Image from 'next/image'
 import placeholder from '../public/images/sobre.jpg'
+import YoutubeVideo from './YoutubeVideo'
 
 export default function Sobre(props) {
     return(
         <section id="Sobre" className={styles.section}>
-            <div className={styles.areaImagem}>
-                <Image alt="Imagem sobre a infecção hospitalar" layout="fill" src={placeholder} objectFit="cover"/>
-            </div>
+            <YoutubeVideo title="Video Contis" url="https://www.youtube.com/embed/orbUSA7pbXY" />
             <div className={styles.areaInfo}>
                 <h2 className={styles.title}>O CONTIS</h2>
                 <p className={styles.p}>O grupo Onfinity tem o prazer em apresentar o 1º  Congresso Sul Brasileiro de Inovação em Saúde. Com tema voltado à Segurança, Tecnologia aliadas à Sustentabilidade, buscamos a prevenção e controle de Infecções Hospitalares. A partir do viés científico, este evento propõe a discussão, sensibilização e aperfeiçoamento profissional do público presente por meio de palestras e mesas-redondas.</p>
